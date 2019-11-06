70059bb7e4eab99809d63e367edd767fee1845f8
hiroyuki.inuyama <kaigaigame34@gmail.com>
2019-11-06 13:08:23 +0000

    user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:index, :show]
+
   def index
     @users = User.order(id: :desc).page(params[:page]).per(25)
   end
@@ -12,7 +14,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params_user)
+    @user = User.new(user_params)
     if @user.save
       flash[:success] = "ユーザの登録に成功しました"
       redirect_to @user
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
-    root to:  'posts#index'
-   
-   get 'signup', to: 'users#new'
-    resources :users, only: [:index, :show, :new, :create]
-    resources :posts
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'posts#index'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create,] 
+  resources :posts, only: [:create, :destroy]
+ 
+  resources :posts
 end
